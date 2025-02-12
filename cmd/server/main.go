3bbf6daf7b17ffac481902baeec42830d21b29b5
Fepozopo <fepozopo.f39@gmail.com>
2025-02-11 19:18:01 -0800

    Updated cmd/server/main.go to connect to/disconnect from the server and updated rabbit.sh to allow for starting/stopping rabbitmq-stomp

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	// Declare a connection string
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ!")
+
+	// Wait for a signal to exit and if received, close the connection
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	<-signalChannel
+
+	fmt.Println("\nGoodbye!")
 }
--- rabbit.sh
+++ rabbit.sh
@@ -13,6 +13,18 @@ case "$1" in
         echo "Fetching logs for RabbitMQ container..."
         docker logs -f rabbitmq
         ;;
+    start-stomp)
+        echo "Starting RabbitMQ STOMP container..."
+        docker run -d --rm --name rabbitmq-stomp -p 5672:5672 -p 15672:15672 -p 61613:61613 rabbitmq-stomp
+        ;;
+    stop-stomp)
+        echo "Stopping RabbitMQ STOMP container..."
+        docker stop rabbitmq-stomp
+        ;;
+    logs-stomp)
+        echo "Fetching logs for RabbitMQ STOMP container..."
+        docker logs -f rabbitmq-stomp
+        ;;
     *)
         echo "Usage: $0 {start|stop|logs}"
         exit 1
