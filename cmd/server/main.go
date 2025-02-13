47b1ca33ff814d18831a65aa2735c917d5199c03
Fepozopo <fepozopo.f39@gmail.com>
2025-02-12 18:34:17 -0800

    Created PublishJSON function and updated cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -18,7 +20,22 @@ func main() {
 	}
 	defer conn.Close()
 
+	// Create a new channel
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
 	fmt.Println("Connected to RabbitMQ!")
+
+	// Publish a message to the exchange
+	ps := routing.PlayingState{
+		IsPaused: true,
+	}
+	if err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, ps); err != nil {
+		panic(err)
+	}
 
 	// Wait for a signal to exit and if received, close the connection
 	signalChannel := make(chan os.Signal, 1)
