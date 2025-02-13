3776ced6da802019727eb972685ab553f7352880
Fepozopo <fepozopo.f39@gmail.com>
2025-02-12 20:33:57 -0800

    Updated cmd/client and created internal/pubsub/declare_and_bind.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	// Declare a connection string
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// Create a new channel
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Hello, %s! Let's start the game!\n", username)
+
+	// Call DeclareAndBind
+	_, _, err = pubsub.DeclareAndBind(
+		conn,                          // conn
+		routing.ExchangePerilDirect,   // exchange
+		routing.PauseKey+"."+username, // queueName
+		routing.PauseKey,              // key
+		pubsub.Transient,              // simpleQueueType
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Wait for a signal to exit and if received, close the connection
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	<-signalChannel
+
+	fmt.Println("\nGoodbye!")
 }
---
+++ internal/pubsub/declare_and_bind.go
@@ -0,0 +1,74 @@
+package pubsub
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+// Define queue types.
+const (
+	Durable = iota
+	Transient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType int, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	// Create a new channel
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	// Set queue declare parameters based on simpleQueueType.
+	// For a durable queue: durable = true. For a transient queue:
+	// autoDelete and exclusive should be true.
+	var durable, autoDelete, exclusive bool
+	if simpleQueueType == Durable {
+		durable = true
+		autoDelete = false
+		exclusive = false
+	} else if simpleQueueType == Transient {
+		durable = false
+		autoDelete = true
+		exclusive = true
+	} else {
+		ch.Close()
+		return nil, amqp.Queue{}, fmt.Errorf("unknown simpleQueueType: %d", simpleQueueType)
+	}
+
+	// Declare the queue.
+	// noWait is false so we wait for a confirmation, and arguments is nil.
+	queue, err := ch.QueueDeclare(
+		queueName,  // queue name
+		durable,    // durable - stays available after broker restart if true
+		autoDelete, // autoDelete - deleted when last consumer disconnects if true
+		exclusive,  // exclusive - only accessible by the current connection if true
+		false,      // noWait - false, wait for a server reply
+		nil,        // arguments
+	)
+	if err != nil {
+		ch.Close()
+		return nil, amqp.Queue{}, fmt.Errorf("declaring queue: %v", err)
+	}
+
+	// Bind the queue to the exchange.
+	err = ch.QueueBind(
+		queue.Name, // queue name
+		key,        // routing key
+		exchange,   // exchange
+		false,      // noWait - false, wait for a server reply
+		nil,        // arguments
+	)
+	if err != nil {
+		ch.Close()
+		return nil, amqp.Queue{}, fmt.Errorf("binding queue: %v", err)
+	}
+
+	return ch, queue, nil
+}
